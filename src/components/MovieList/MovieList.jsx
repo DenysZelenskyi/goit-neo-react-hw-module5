28e5f1eff468a3a432064d6a3a63b574f4d4230d
DenysZelenskyi <den.zelenskiy@gmail.com>
2025-03-11 23:30:00 -0700

    add vercel.json

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,16 +1,15 @@
-import { Link, useLocation } from 'react-router-dom';
-import PropTypes from 'prop-types';
-import styles from './MovieList.module.css';
+import { Link, useLocation } from "react-router-dom";
+import styles from "./MovieList.module.css";
 
 const MovieList = ({ movies }) => {
   const location = useLocation();
 
   return (
     <ul className={styles.movieList}>
-      {movies.map(movie => (
+      {movies.map((movie) => (
         <li key={movie.id}>
-          <Link 
-            to={`/movies/${movie.id}`} 
+          <Link
+            to={`/movies/${movie.id}`}
             state={{ from: location }}
             className={styles.list_style}
           >
@@ -20,15 +19,6 @@ const MovieList = ({ movies }) => {
       ))}
     </ul>
   );
-};
-
-MovieList.propTypes = {
-  movies: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      title: PropTypes.string.isRequired,
-    })
-  ).isRequired,
 };
 
 export default MovieList;
